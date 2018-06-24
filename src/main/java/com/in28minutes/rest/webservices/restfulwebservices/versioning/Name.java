b1129f77544d69311da435e6b71818e34ec4c976
Everton Silva <everton0189@gmail.com>
2018-06-24 20:24:17 -0300

    Versioning RESTful Services - Basic Approach with URIs

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/Name.java
@@ -0,0 +1,17 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Name {
+
+    private String firstName;
+    private String lastName;
+
+}
